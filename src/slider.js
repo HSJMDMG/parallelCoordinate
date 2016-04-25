0e89c0bb38be0e3eef063543b009309670d4f9f8
HSJMDMG <lynj.y@qq.com>
2016-04-25 21:43:55 +0800

    Add draggable axis
    enable reordering of dimensions

--- src/slider.js
+++ src/slider.js
@@ -9,18 +9,49 @@ var myApp = angular.module('myApp', []);
                     var width = 1000;
                     var height = 600;
                     var padding = 50;
+                    var innerWidth = width - 2 * padding;
                     var keynum = dataset[0] ? Object.keys(dataset[0]).length: 0; 
                     var color = d3.scale.category10();
                     var colornum =  10;  //10/20
                     var strokewidthscale = 10;
                     var opacityscale = 100;
                     var dragoffset = 25;
+                    var dimensions = Object.keys(dataset[0]);
                     var lineFunction = d3.svg.line()
                                       .x(function(d) { return d.x;})
                                       .y(function(d) { return d.y;})
                                      .interpolate("linear");    
-
-
+                    
+                    
+                    
+                    
+                    console.log(dataset[0]);
+                    var Xdomain = new Array();
+                    var i = 0;
+                    for (attr in dataset[0])
+                    {
+                        Xdomain.push(padding + 1.0 * i * (width - 2 * padding)/(keynum - 1));
+                        i++;
+                    }
+                    
+                    console.log('Xdomain')
+                    console.log(Xdomain);
+                    //!!!最左边最右边有点歪
+                   /* var ordinal = d3.scale.ordinal().domain([1,2,3,4,5]).rangePoints([0,100]);
+                    console.log('ordinal')
+                    console.log( ordinal.range());
+                    ordinal.rangePoints([padding, width - padding],5);
+                    console.log( ordinal.range() );
+                    console.log(padding);
+                    console.log(width);
+                    */
+                    var x = d3.scale.ordinal().domain(Xdomain);
+                    x.rangePoints([padding, width - padding]);
+                    
+                    
+                    
+                    console.log(x.range());
+                    console.log('aha')
                     var svg = d3.select('#chart').append('svg')
                                                 .attr('width',width)
                                                 .attr('height',height);
@@ -29,15 +60,24 @@ var myApp = angular.module('myApp', []);
                     svg.append('g')
                     .attr('id','axisSet');
 
+                    
+                    var xScale = d3.scale.linear()
+                                    .domain([0, width])
+                                    .range([0, width]);
+                    var yScale = d3.scale.linear()
+                                    .domain([0, height])
+                                    .range([0, height]);
+                    
+                    
                     var brush = d3.svg.brush()  //点击拖拽选择一个二维区域。
-                                    .x(x2)
-                                    .y(y2)
+                                    .x(xScale)
+                                    .y(yScale)
                                     .on('brush', onBrush);
                     
                     function onBrush() {
                         var extent = brush.extent();
                         /* 调整相应图1的x轴坐标和y轴坐标范围 */
-                       
+                       console.log(extent);
                        
                     }
                     
@@ -77,10 +117,33 @@ var myApp = angular.module('myApp', []);
 
                      function DrawAxis() {
                           //Axis
+                         
+                         
                         var axis = svg.select('#axisSet').selectAll('.axis')
                                          .data(Object.keys(dataset[0])); //mapping axis per keys in the dataset (ie 'a','b','c'...)
+                        
+                        var line = d3.svg.line();
                     
                          /*Drag*/
+                         //?????
+                         var dragging = {};
+
+                        function path(d) {
+                            return line(axis.map(function(p) { 
+                                    return [position(p), axis[p](d[p])]; 
+                                    }));
+                        }
+
+
+                        function position(d) {
+                            
+                            // if we're currently dragging the axis return the drag position
+                            // otherwise return the normal x-axis position
+                            var v = dragging[d];
+                            return v == null ? x(d) : v;
+                        }
+                         
+                         
                         function dragmove(d) {
                             //console.log(d);
                            //为什么是列名？！
@@ -88,10 +151,30 @@ var myApp = angular.module('myApp', []);
                             //console.log('axis[i]');
                             //console.log(i);
                             //console.log(axis[i]);
-                            //console.log(d3.event.x);
+                            console.log(d3.event.x);
                         //    d.dx = d3.event.x;
                             d3.select(this)
                                     .attr('transform', 'translate('+d3.event.x+')');
+                            
+                            
+                            //??????????????????????????????
+                            dragging[d] = d3.event.x;
+
+                            console.log('darggingd');
+                            console.log(dragging[d]);
+                            
+                            svg.select('#lineSet').selectAll('.lines').attr('d', path);
+                            dimensions.sort(function(a, b) { return position(a) - position(b); });
+                          //  console.log('dimension')
+                        //    console.log(dimensions);
+                            
+                            x.domain(dimensions);
+                            console.log(svg.select('g.axisSet'));
+                            svg.select('#axisSet').selectAll('.axis').attr('transform', function(d) { 
+                          /*      console.log('d+posd');
+                                console.log(d);
+                                console.log(position(d));*/
+                                return 'translate(' + position(d) + ')'; });
                         }
                       
                         function dragstart(d) {
@@ -107,6 +190,11 @@ var myApp = angular.module('myApp', []);
                                 .select('path')
                                 .attr('fill', '#66CCFF')
                                 .attr('opacity', 0.8);
+                            console.log('start');
+                            //console.log(d3.event.dx);
+                            dragging[d] = this.__origin__ = x(d);
+                            console.log(dragging[d]);
+                            
                         }
                          
                         function dragend(d) {
@@ -116,6 +204,21 @@ var myApp = angular.module('myApp', []);
                                     .select('path')
                                     .attr('fill', 'null')
                                     .attr('opacity', 1);
+                            
+                            delete this.__origin__;
+                            delete dragging[d];
+                            console.log('x-d');
+                            console.log(x(d));
+                            d3.select(this).attr('transform', 'translate(' + x(d) + ')');
+                            
+                            
+                            svg.select('.lineSet').select('.lines').attr('d', path);
+                            
+                            
+                            function ReOrderAxis() {
+                                
+                                
+                            }
                         
                         function UpdateDataset(d)
                         {
@@ -123,11 +226,13 @@ var myApp = angular.module('myApp', []);
                         }
                             UpdateDataset(d);
                        
-                            DrawAxis();
-                            DrawLine();
+                          //  DrawAxis();
+                        //    DrawLine();
                         }
                          
                          
+      
+                                             
                         var drag = d3.behavior.drag()
                                         .origin(null)
                                         .on('drag', dragmove)
